d75986a25f90d9611e0d56c6450904123842c760
Simon Lloyd <smlloyd@gmail.com>
2024-03-08 13:54:45 +0000

    zsh tweaks

--- dot_bashrc.d/starship.sh
+++ dot_bashrc.d/starship.sh
@@ -1,3 +1,5 @@
-if command -v starship 1>/dev/null 2>&1; then
-	eval "$(starship init bash)"
+if command -v starship  1>/dev/null 2>&1; then
+  eval "$(starship init bash)"
+elif command -v $(mise which starship) 1>/dev/null 2>&1;then
+  eval "$(mise exec starship -- starship init bash)"
 fi
--- run_after_zsh_abbr.sh
+++ run_after_zsh_abbr.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/zsh
 
-eval "(zsh -ci abbr import-aliases 1> /dev/null)"
+eval "(zsh -cli abbr import-aliases 1> /dev/null)"
